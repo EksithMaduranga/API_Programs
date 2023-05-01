39ff52acddcb9d7e1a57f904216c12d0c21741ac
EksithMaduranga <eksithmadu9@gmail.com>
2023-05-01 17:43:24 +0530

    initial commit

---
+++ Sliit.MTIT.Student/Controllers/StudentController.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Mvc;
+using Sliit.MTIT.Student.Data;
+using Sliit.MTIT.Student.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Sliit.MTIT.Student.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentService _studentService;
+
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService ?? throw new ArgumentNullException(nameof(studentService));    
+        }
+
+        // GET: api/<StudentController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_studentService.GetStudents());
+        }
+
+        // GET api/<StudentController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return _studentService.GetStudent(id) != null ? Ok(_studentService.GetStudent(id)) : NoContent();
+        }
+
+        // POST api/<StudentController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Models.Student student)
+        {
+            return Ok(_studentService.AddStudent(student));
+        }
+
+        // PUT api/<StudentController>/5
+        [HttpPut]
+        public IActionResult Put([FromBody] Models.Student student)
+        {
+            return Ok(_studentService.UpdateStudent(student));
+        }
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var result = _studentService.DeleteStudent(id);
+
+            return result.HasValue & result == true ? Ok($"student with ID:{id} got deleted successfully.")
+                : BadRequest($"Unable to delete the studend with ID:{id}.");
+        }
+    }
+}
---
+++ Sliit.MTIT.Student/Data/StudentMockDataService.cs
@@ -0,0 +1,15 @@
+﻿namespace Sliit.MTIT.Student.Data
+{
+    public static class StudentMockDataService
+    {
+        public static List<Models.Student> Students = new List<Models.Student>()
+        {
+            new Models.Student { Id = 1, Name = "Dinesh", Address = "Colombo", Age = 22 },
+            new Models.Student { Id = 2, Name = "Kumar", Address = "Galle", Age = 21 },
+            new Models.Student { Id = 3, Name = "Ravi", Address = "Jaffna", Age = 23 },
+            new Models.Student { Id = 4, Name = "Kamal", Address = "Colombo", Age = 20 },
+            new Models.Student { Id = 5, Name = "Pavi", Address = "Kandy", Age = 23 }
+        };
+
+    }
+}
